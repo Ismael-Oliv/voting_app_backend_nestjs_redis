ccd4f359f0344124d5ea2aed441aaa8e4be53194
Ismael <ismae@pop-os.localdomain>
2022-12-27 07:19:15 -0300

    first commit

--- src/polls/controller/polls.controller.ts
+++ src/polls/controller/polls.controller.ts
@@ -6,14 +6,11 @@ import {
   Req,
   UsePipes,
   ValidationPipe,
-  Get,
 } from '@nestjs/common';
 import { PollsService } from '../services/polls.services';
 import { CreatePollDto, JoinPollDto } from '../dtos';
 import { ControllerAuthGuard } from '../guards/cotroller-auth.guard';
 import { RequestWithAuth } from '../types';
-import { ConnectedSocket } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
 
 @UsePipes(new ValidationPipe())
 @Controller('polls')
